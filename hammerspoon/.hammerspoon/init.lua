1fbd3275dcece28706e6a9dd5413f36c50f28826
Thomas Price <taprice@thecubicle.ca>
2018-01-15 10:12:40 -0500

    Redid window positioning to cycle through various sizes to work on both ultrawide and standard aspect ratios

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -5,7 +5,8 @@
 local hyper = {"shift", "cmd", "alt", "ctrl"}
 
 -----------------------------------------------
--- hyper left for left one half window
+-- hyper left to cycle between 
+-- left 1/2, 1/3, 1/4
 -----------------------------------------------
 
 hs.hotkey.bind(hyper, "left", function()
@@ -14,11 +15,13 @@ hs.hotkey.bind(hyper, "left", function()
     local screen = win:screen()
     local max = screen:frame()
     local sections
-
-    if f.w == math.floor(max.w / 3) then
-        sections = 4
-    else
+	
+    if f.w >= math.floor(max.w / 2) then
         sections = 3
+    elseif f.w >= math.floor(max.w / 3) then
+		sections = 4
+	else
+		sections = 2
     end
 
     f.x = 0 
@@ -29,7 +32,8 @@ hs.hotkey.bind(hyper, "left", function()
 end)
 
 -----------------------------------------------
--- hyper right for right one half window
+-- hyper right to cycle between
+-- right 1/2, 1/3, 1/4
 -----------------------------------------------
 
 hs.hotkey.bind(hyper, "right", function()
@@ -38,12 +42,15 @@ hs.hotkey.bind(hyper, "right", function()
     local screen = win:screen()
     local max = screen:frame()
 
-    if f.x == max.w - math.floor(max.w / 3) then
-        f.x = max.w - (max.w / 4)
-        f.w = max.w / 4
-    else
-        f.x = max.w - math.floor(max.w / 3)
-        f.w = max.w / 2
+    if f.w >= math.floor(max.w / 2) then
+		f.x = max.w - math.floor(max.w / 3)
+		f.w = math.floor(max.w / 3)
+    elseif f.w >= math.floor(max.w / 3) then
+		f.x = max.w - (max.w / 4)
+		f.w = math.floor(max.w / 4)
+	else
+		f.x = math.floor(max.w / 2)
+		f.w = max.w / 2
     end
 
     f.y = max.y
@@ -52,7 +59,8 @@ hs.hotkey.bind(hyper, "right", function()
 end)
 
 -----------------------------------------------
--- hyper down for center
+-- hyper down to cycle through
+-- center 1/2, 1/3
 -----------------------------------------------
 
 hs.hotkey.bind(hyper, "down", function()
@@ -61,15 +69,15 @@ hs.hotkey.bind(hyper, "down", function()
     local screen = win:screen()
     local max = screen:frame()
 
-    if f.w == math.floor(max.w / 2) then
-        f.x = math.floor(max.w / 3) 
-        f.y = 0
-        f.w = math.floor(max.w / 3)
-        f.h = max.h
-    else
+    if f.w <= math.floor(max.w / 3) then
         f.x = math.floor(max.w / 4) 
         f.y = 0
         f.w = math.floor(max.w / 2)
+        f.h = max.h
+    else
+        f.x = math.floor(max.w / 3) 
+        f.y = 0
+        f.w = math.floor(max.w / 3)
         f.h = max.h
     end
 
